8db914fe0992d70626b863f6ecbc611ebea46cbd
Jaqueline Medeiros <alves_medeiros@outlook.com>
2026-05-12 23:58:55 +0000

    Feat: adicionando paginas para emissão de certificado

--- site Scrum do Zero/src/routes/index.js
+++ site Scrum do Zero/src/routes/index.js
@@ -1,17 +1,19 @@
-const { Router } = require("express")
-const usuarios = require("./usuarios.routes")
-const questoes = require("./questoes.routes")
-const auth = require("./auth.routes")
+const { Router } = require("express");
+const usuarios = require("./usuarios.routes");
+const questoes = require("./questoes.routes");
+const auth = require("./auth.routes");
+const certificados = require("./certificados.routes");
 
-const router = Router()
+const router = Router();
 
-router.use("/usuarios", usuarios)
-router.use("/questoes", questoes)
+router.use("/usuarios", usuarios);
+router.use("/questoes", questoes);
+router.use("/certificados", certificados);
 
-router.use("/auth", auth)
+router.use("/auth", auth);
 
 router.use(function(_req,res){
-    res.status(404).json({message: "Rota inexistente"})
+    res.status(404).json({message: "Rota inexistente"});
 })
 
 module.exports = router;
