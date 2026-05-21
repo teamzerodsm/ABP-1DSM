797782b6f86352b33278f50af1dd41e2eb746435
Jaqueline Medeiros <alves_medeiros@outlook.com>
2026-05-20 22:24:00 -0300

    corrigindo erro server

--- site Scrum do Zero/src/server.js
+++ site Scrum do Zero/src/server.js
@@ -1,15 +1,12 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
 const router = require("./routes");
-const progressoRoutes = require('./routes/progresso');
-const cors = require('cors');
-
-app.use(cors());
+const progressoRoutes = require("./routes/progresso.routes");
 
 dotenv.config({
-    quiet: true,
-    path: path.resolve(__dirname, "..", ".env")
+  quiet: true,
+  path: path.resolve(__dirname, "..", ".env"),
 });
 
 const app = express();
@@ -31,39 +28,40 @@ app.use("/", express.static(pagesPath));
 app.use("/assets", express.static(assetsPath));
 app.use("/imagens/questoes", express.static(imagensQuestoesPath));
 app.use("/api", router);
+app.use("/progresso", progressoRoutes);
 
-app.get("/index", function(_req, res){
-    res.sendFile(path.join(pagesPath, "index.html"));
+app.get("/index", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "index.html"));
 });
 
-app.get("/main", function(_req, res){
-    res.sendFile(path.join(pagesPath, "main.html"));
+app.get("/main", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "main.html"));
 });
 
-app.get("/cadastro", function(_req, res){
-    res.sendFile(path.join(pagesPath, "cadastro.html"));
+app.get("/cadastro", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "cadastro.html"));
 });
 
-app.get("/perfil", function(_req, res){
-    res.sendFile(path.join(pagesPath, "perfil.html"));
+app.get("/perfil", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "perfil.html"));
 });
 
-app.get("/progresso", function(_req, res){
-    res.sendFile(path.join(pagesPath, "progresso.html"));
+app.get("/progresso", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "progresso.html"));
 });
 
-app.get("/quiz-page", function(_req, res){
-    res.sendFile(path.join(pagesPath, "quiz-page.html"));
+app.get("/quiz-page", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "quiz-page.html"));
 });
 
-app.get("/certificado", function(_req, res){
-    res.sendFile(path.join(pagesPath, "certificado.html"));
+app.get("/certificado", function (_req, res) {
+  res.sendFile(path.join(pagesPath, "certificado.html"));
 });
 
-app.use(function(_req,res){
-    res.redirect("not-found.html");
+app.use(function (_req, res) {
+  res.redirect("not-found.html");
 });
 
 app.listen(PORT, function () {
-    console.log(`Rodando em http://localhost:${PORT}`);
+  console.log(`Rodando em http://localhost:${PORT}`);
 });
